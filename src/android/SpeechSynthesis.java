cb44f2b96ffc8971868358482e5a4b6974d5a603
martin bedouret <martinbedouret@gmail.com>
2021-03-04 17:15:04 -0300

    wip

--- src/android/SpeechSynthesis.java
+++ src/android/SpeechSynthesis.java
@@ -37,7 +37,7 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
 
     private Set<Voice> voiceList = null;
 
-    //private String startupCallbackId = "";
+    // private String startupCallbackId = "";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
@@ -64,29 +64,29 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
                     for (Voice v : this.voiceList) {
                         if (voiceCode.equals(v.getName())) {
                             mTts.setVoice(v);
-                            //text+=" yay! found the voice!";
+                            // text+=" yay! found the voice!";
                         }
                     }
                 }
 
-                float pitch = (float)utterance.optDouble("pitch", 1.0);
+                float pitch = (float) utterance.optDouble("pitch", 1.0);
                 mTts.setPitch(pitch);
 
-                float volume = (float)utterance.optDouble("volume", 0.5);
+                float volume = (float) utterance.optDouble("volume", 0.5);
                 // how to set volume
-                
-                float rate = (float)utterance.optDouble("rate", 1.0);
+
+                float rate = (float) utterance.optDouble("rate", 1.0);
                 mTts.setSpeechRate(rate);
-                
+
                 if (isReady()) {
                     HashMap<String, String> map = null;
                     map = new HashMap<String, String>();
                     map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, callbackContext.getCallbackId());
                     JSONObject event = new JSONObject();
-                    event.put("type","start");
-                    event.put("charIndex",0);
-                    event.put("elapsedTime",0);
-                    event.put("name","");
+                    event.put("type", "start");
+                    event.put("charIndex", 0);
+                    event.put("elapsedTime", 0);
+                    event.put("name", "");
                     PluginResult pr = new PluginResult(PluginResult.Status.OK, event);
                     pr.setKeepCallback(true);
                     callbackContext.sendPluginResult(pr);
@@ -98,7 +98,7 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
                 if (isReady()) {
                     HashMap<String, String> map = null;
                     map = new HashMap<String, String>();
-                    //map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, callbackId);
+                    // map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, callbackId);
                     mTts.speak("", TextToSpeech.QUEUE_FLUSH, map);
                     fireEndEvent(callbackContext);
                 } else {
@@ -132,23 +132,20 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
                 if (mTts == null) {
                     state = SpeechSynthesis.INITIALIZING;
                     mTts = new TextToSpeech(cordova.getActivity().getApplicationContext(), this);
-                }else{
-            		getVoices(callbackContext);
+                } else {
+                    getVoices(callbackContext);
                 }
                 PluginResult pluginResult = new PluginResult(status, SpeechSynthesis.INITIALIZING);
                 pluginResult.setKeepCallback(true);
                 startupCallbackContext.sendPluginResult(pluginResult);
             }
 
-			
-			
             else if (action.equals("shutdown")) {
                 if (mTts != null) {
                     mTts.shutdown();
                 }
                 callbackContext.sendPluginResult(new PluginResult(status, result));
-            }
-            else if (action.equals("isLanguageAvailable")) {
+            } else if (action.equals("isLanguageAvailable")) {
                 if (mTts != null) {
                     Locale loc = new Locale(args.getString(0));
                     int available = mTts.isLanguageAvailable(loc);
@@ -166,7 +163,7 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
     private void getVoices(CallbackContext callbackContext) {
         JSONArray voices = new JSONArray();
         JSONObject voice;
-        //List<TextToSpeech.EngineInfo> engines = mTts.getEngines();
+        // List<TextToSpeech.EngineInfo> engines = mTts.getEngines();
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
             this.voiceList = mTts.getVoices();
@@ -176,9 +173,10 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
                 try {
                     voice.put("voiceURI", v.getName());
                     voice.put("name", locale.getDisplayLanguage(locale) + " " + locale.getDisplayCountry(locale));
-                    //voice.put("features", v.getFeatures());
-                    //voice.put("displayName", locale.getDisplayLanguage(locale) + " " + locale.getDisplayCountry(locale));
-                    voice.put("lang", locale.getLanguage()+"-"+locale.getCountry());
+                    // voice.put("features", v.getFeatures());
+                    // voice.put("displayName", locale.getDisplayLanguage(locale) + " " +
+                    // locale.getDisplayCountry(locale));
+                    voice.put("lang", locale.getLanguage() + "-" + locale.getCountry());
                     voice.put("localService", !v.isNetworkConnectionRequired());
                     voice.put("quality", v.getQuality());
                     voice.put("default", false);
@@ -187,20 +185,20 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
                 }
                 voices.put(voice);
             }
-        }else{
-            //Iterator<Locale> list = voiceList.iterator();
+        } else {
+            // Iterator<Locale> list = voiceList.iterator();
             Locale[] list = Locale.getAvailableLocales();
             Locale locale;
-            //while (list.hasNext()) {
-            //    locale = list.next();
+            // while (list.hasNext()) {
+            // locale = list.next();
             for (int i = 0; i < list.length; i++) {
                 locale = list[i];
                 voice = new JSONObject();
-                if (mTts.isLanguageAvailable(locale) > 0) {     // ie LANG_COUNTRY_AVAILABLE or LANG_COUNTRY_VAR_AVAILABLE
+                if (mTts.isLanguageAvailable(locale) > 0) { // ie LANG_COUNTRY_AVAILABLE or LANG_COUNTRY_VAR_AVAILABLE
                     try {
-                        voice.put("voiceURI", locale.getLanguage()+"-"+locale.getCountry());
+                        voice.put("voiceURI", locale.getLanguage() + "-" + locale.getCountry());
                         voice.put("name", locale.getDisplayLanguage(locale) + " " + locale.getDisplayCountry(locale));
-                        voice.put("lang", locale.getLanguage()+"-"+locale.getCountry());
+                        voice.put("lang", locale.getLanguage() + "-" + locale.getCountry());
                         voice.put("localService", true);
                         voice.put("default", false);
                     } catch (JSONException e) {
@@ -219,7 +217,7 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
     private void fireEndEvent(CallbackContext callbackContext) {
         JSONObject event = new JSONObject();
         try {
-            event.put("type","end");
+            event.put("type", "end");
         } catch (JSONException e) {
             // this will never happen
         }
@@ -228,13 +226,12 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
         callbackContext.sendPluginResult(pr);
     }
 
-    private void fireErrorEvent(CallbackContext callbackContext)
-            throws JSONException {
+    private void fireErrorEvent(CallbackContext callbackContext) throws JSONException {
         JSONObject error = new JSONObject();
-        error.put("type","error");
-        error.put("charIndex",0);
-        error.put("elapsedTime",0);
-        error.put("name","");
+        error.put("type", "error");
+        error.put("charIndex", 0);
+        error.put("elapsedTime", 0);
+        error.put("name", "");
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, error));
     }
 
@@ -256,36 +253,35 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
         if (mTts != null && status == TextToSpeech.SUCCESS) {
             state = SpeechSynthesis.STARTED;
             getVoices(this.startupCallbackContext);
-            
-            
-//                Putting this code in hear as a place holder. When everything moves to API level 15 or greater
-//                we'll switch over to this way of tracking progress.
-//                mTts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
-//
-//                    @Override
-//                    public void onDone(String utteranceId) {
-//                        Log.d(LOG_TAG, "got completed utterance");
-//                        PluginResult result = new PluginResult(PluginResult.Status.OK);
-//                        result.setKeepCallback(false);
-//                        callbackContext.sendPluginResult(result);        
-//                    }
-//
-//                    @Override
-//                    public void onError(String utteranceId) {
-//                        Log.d(LOG_TAG, "got utterance error");
-//                        PluginResult result = new PluginResult(PluginResult.Status.ERROR);
-//                        result.setKeepCallback(false);
-//                        callbackContext.sendPluginResult(result);        
-//                    }
-//
-//                    @Override
-//                    public void onStart(String utteranceId) {
-//                        Log.d(LOG_TAG, "started talking");
-//                    }
-//                    
-//                });
-        }
-        else if (status == TextToSpeech.ERROR) {
+
+            // Putting this code in hear as a place holder. When everything moves to API
+            // level 15 or greater
+            // we'll switch over to this way of tracking progress.
+            // mTts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            //
+            // @Override
+            // public void onDone(String utteranceId) {
+            // Log.d(LOG_TAG, "got completed utterance");
+            // PluginResult result = new PluginResult(PluginResult.Status.OK);
+            // result.setKeepCallback(false);
+            // callbackContext.sendPluginResult(result);
+            // }
+            //
+            // @Override
+            // public void onError(String utteranceId) {
+            // Log.d(LOG_TAG, "got utterance error");
+            // PluginResult result = new PluginResult(PluginResult.Status.ERROR);
+            // result.setKeepCallback(false);
+            // callbackContext.sendPluginResult(result);
+            // }
+            //
+            // @Override
+            // public void onStart(String utteranceId) {
+            // Log.d(LOG_TAG, "started talking");
+            // }
+            //
+            // });
+        } else if (status == TextToSpeech.ERROR) {
             state = SpeechSynthesis.STOPPED;
             PluginResult result = new PluginResult(PluginResult.Status.ERROR, SpeechSynthesis.STOPPED);
             result.setKeepCallback(false);
@@ -303,7 +299,8 @@ public class SpeechSynthesis extends CordovaPlugin implements OnInitListener, On
     }
 
     /**
-     * Once the utterance has completely been played call the speak's success callback
+     * Once the utterance has completely been played call the speak's success
+     * callback
      */
     public void onUtteranceCompleted(String utteranceId) {
         fireEndEvent(callbackContext);
